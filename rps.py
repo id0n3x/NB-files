04b17ae55ed854ad328b930c357fdb59bbc45751
id0n3x <80592269+id0n3x@users.noreply.github.com>
2021-09-16 00:24:22 +0300

    Update rps.py

--- rps.py
+++ rps.py
@@ -97,7 +97,7 @@ def updateCb(client, callback_query,redis):
       InlineKeyboardButton("📃",callback_data="st2={}={}=1={}".format(user1,user2,chs)),
       InlineKeyboardButton("✂️",callback_data="st2={}={}=2={}".format(user1,user2,chs)),],
 
-      [InlineKeyboardButton("📣",url="t.me/nbbot")]
+      [InlineKeyboardButton("📣",url="t.me/calmaacc")]
       ])
     Bot("editMessageText",{"chat_id":chatID,"message_id":message_id,"text":go.format("✅",userFN,"⏺",userFn, userFn),"disable_web_page_preview":True,"reply_markup":kb})
 
@@ -154,7 +154,7 @@ def updateCb(client, callback_query,redis):
       InlineKeyboardButton("📃",callback_data="st1={}={}=1".format(userid,userID)),
       InlineKeyboardButton("✂️",callback_data="st1={}={}=2".format(userid,userID)),],
 
-      [InlineKeyboardButton("📣",url="t.me/nbbot")]
+      [InlineKeyboardButton("📣",url="t.me/calmaacc")]
       ])
 
     Bot("editMessageText",{"chat_id":chatID,"message_id":message_id,"text":go.format("⏺",userFn,"⏺",userFN, userFn),"disable_web_page_preview":True,"reply_markup":kb})
